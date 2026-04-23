76935662df01b4f793562f8ca0887fbe620537b3
mokuno3430 <mokuno3430@gmail.com>
2026-04-23 09:16:14 +0900

    Reorganize sample data and update example files

--- sample_data/a_Stx-phages/runme.sh
+++ sample_data/a_Stx-phages/runme.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-prefix=Stx-phage_loci
-a-liner --xlsx seq_info.xlsx \
-        --blastn blastn_outfmt6.txt \
-        --seq_color black \
-        --gff3 gff_files/*.gff3 \
-        --gff_xlsx gff_files/cladeI_10290_gff.xlsx \
-        --feature CDS pseudogene \
-        --gene_edge_color black \
-        --scale tick \
-        --highlight highlights.txt \
-        --h_alpha 0.5 \
-        --h_thickness 4 \
-        --colormap 0 \
-        --left_margin 0.25 \
-	    --out ${prefix} 1> log_${prefix}
+prefix=Stx-prophage-regions 
+a-liner --xlsx input/sequence_config.xlsx \
+        --seq_thickness 0.75 --seq_color black --scale tick \
+        --blastn input/blastn_outfmt6.txt \
+        --min_identity 80 --alignment_alpha 0.35 --colormap 0 \
+        --gb input/*.gb --gff3 input/*.gff --gene_thickness 6 \
+        --feature_color_map input/color_legend_map.txt \
+        --feature CDS pseudogene --gene_edge_color black \
+        --scatter input/GCcontent_files/*_GCcontent_w1000-s100.txt \
+        --scatter_min 30 --scatter_max 70 --scatter_space 0.6 --scatter_ylines 30 50 70 \
+        --marker_color "#c33399" --background_color "white" \
+        --highlight input/highlights.txt --h_alpha 0.5 --h_thickness 8 \
+	    --out ${prefix} 1> ${prefix}.log
